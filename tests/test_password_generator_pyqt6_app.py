e1a857cf27d6c4787d8c5ef7d3505a150646111d
angelsg06 <segoa587@hsd.k12.or.us>
2023-04-07 13:17:38 -0700

    Created widget variables for app

--- tests/test_password_generator_pyqt6_app.py
+++ tests/test_password_generator_pyqt6_app.py
@@ -13,6 +13,21 @@ class PasswordGenerator(QtWidgets.QMainWindow):
         self.setCentralWidget(central_widget)
 
         # create a vertical box layout for the central widget
+        layout = QtWidgets.QVBoxLayout(central_widget)
+
+        # creates the widget variables for: labels, spinbox, checkboxes, buttons
+        password_length_label = QtWidgets.QLabel("Password Length(1-15:")
+        self.password_length = QtWidgets.QSpinBox()
+        self.include_lowercase = QtWidgets.QCheckBox("Include Lowercase Letters")
+        self.include_uppercase = QtWidgets.QCheckBox("Include Uppercase Letters")
+        self.include_numbers = QtWidgets.QCheckBox("Include Numbers")
+        self.include_symbols = QtWidgets.QCheckBox("Include Symbols")
+        generate_button = QtWidgets.QPushButton("Generate Password")
+        clear_button = QtWidgets.QPushButton("Clear")
+        self.password_display = QtWidgets.QLabel()
+
+        
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
