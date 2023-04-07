d91c15da499bf8108ce6c120919d4ea8d017294a
angelsg06 <segoa587@hsd.k12.or.us>
2023-04-07 12:30:40 -0700

    Created main window for app

--- tests/test_password_generator_pyqt6_app.py
+++ tests/test_password_generator_pyqt6_app.py
@@ -1,17 +1,22 @@
 import sys
-import random
-import string
-from PyQt6 import QtWidgets
+from PyQt6 import QtWidgets, QtCore, QtGui
 
 # Subclass QMainWindow to customize your application's main window
-class PasswordGenerator(QtWidgets.QtWidget):
+class PasswordGenerator(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
+        # set window title
+        self.setWindowTitle("Password Generator")
+        
+        # Create a central widget that contains all the elements
+        central_widget = QtWidgets.QWidget()
+        self.setCentralWidget(central_widget)
 
+        # create a vertical box layout for the central widget
 
-app = QApplication(sys.argv)
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
 
-window = MainWindow()
-window.show()
-
-app.exec()
+    window = PasswordGenerator()
+    window.show()
+    sys.exit(app.exec())
