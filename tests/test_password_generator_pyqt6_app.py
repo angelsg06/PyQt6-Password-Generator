456a995ec71044ea1cc668d71d216c50cf2f3385
angelsg06 <segoa587@hsd.k12.or.us>
2023-04-11 13:12:17 -0700

    Improved variable names + added clear field

--- tests/test_password_generator_pyqt6_app.py
+++ tests/test_password_generator_pyqt6_app.py
@@ -17,25 +17,35 @@ class PasswordGenerator(QtWidgets.QMainWindow):
 
         # creates the widget variables for: labels, spinbox, checkboxes, buttons
         password_length_label = QtWidgets.QLabel("Password Length(1-15):")
-        self.password_length = QtWidgets.QSpinBox()
-        self.include_lowercase = QtWidgets.QCheckBox("Include Lowercase Letters")
-        self.include_uppercase = QtWidgets.QCheckBox("Include Uppercase Letters")
-        self.include_numbers = QtWidgets.QCheckBox("Include Numbers")
-        self.include_symbols = QtWidgets.QCheckBox("Include Symbols")
+        self.password_length_spinbox = QtWidgets.QSpinBox()
+        self.include_lowercase_checkbox = QtWidgets.QCheckBox("Include Lowercase Letters")
+        self.include_uppercase_checkbox = QtWidgets.QCheckBox("Include Uppercase Letters")
+        self.include_numbers_checkbox = QtWidgets.QCheckBox("Include Numbers")
+        self.include_symbols_checkbox = QtWidgets.QCheckBox("Include Symbols")
         generate_button = QtWidgets.QPushButton("Generate Password")
         clear_button = QtWidgets.QPushButton("Clear")
-        self.password_display = QtWidgets.QLabel()
+        self.password_display_label = QtWidgets.QLabel()
+        clear_button.clicked.connect(self.clear_fields)
+
+        self.password_length_spinbox.setRange(1,15)
 
         # Adds the widgets to the layout
         layout.addWidget(password_length_label)
-        layout.addWidget(self.password_length)
-        layout.addWidget(self.include_lowercase)
-        layout.addWidget(self.include_uppercase)
-        layout.addWidget(self.include_numbers)
-        layout.addWidget(self.include_symbols)
+        layout.addWidget(self.password_length_spinbox)
+        layout.addWidget(self.include_lowercase_checkbox)
+        layout.addWidget(self.include_uppercase_checkbox)
+        layout.addWidget(self.include_numbers_checkbox)
+        layout.addWidget(self.include_symbols_checkbox)
         layout.addWidget(generate_button)
         layout.addWidget(clear_button)
-        layout.addWidget(self.password_display)
+        layout.addWidget(self.password_display_label)
+    
+    def clear_fields(self):
+        self.password_length_spinbox.clear()
+        self.include_lowercase_checkbox.setChecked(False)
+        self.include_uppercase_checkbox.setChecked(False)
+        self.include_numbers_checkbox.setChecked(False)
+        self.include_symbols_checkbox.setChecked(False)
 
         
 if __name__ == "__main__":
