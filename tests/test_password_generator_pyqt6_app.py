a28c905bfdfa259f4244f09473505c0f624bd3fa
angelsg06 <segoa587@hsd.k12.or.us>
2023-04-11 13:23:47 -0700

    More comments + start of password generation function

--- tests/test_password_generator_pyqt6_app.py
+++ tests/test_password_generator_pyqt6_app.py
@@ -5,17 +5,17 @@ from PyQt6 import QtWidgets, QtCore, QtGui
 class PasswordGenerator(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
-        # set window title
+        # Set window title
         self.setWindowTitle("Password Generator")
         
         # Create a central widget that contains all the elements
         central_widget = QtWidgets.QWidget()
         self.setCentralWidget(central_widget)
 
-        # create a vertical box layout for the central widget
+        # Create a vertical box layout for the central widget
         layout = QtWidgets.QVBoxLayout(central_widget)
 
-        # creates the widget variables for: labels, spinbox, checkboxes, buttons
+        # Creates the widget variables for: labels, spinbox, checkboxes, buttons
         password_length_label = QtWidgets.QLabel("Password Length(1-15):")
         self.password_length_spinbox = QtWidgets.QSpinBox()
         self.include_lowercase_checkbox = QtWidgets.QCheckBox("Include Lowercase Letters")
@@ -25,8 +25,11 @@ class PasswordGenerator(QtWidgets.QMainWindow):
         generate_button = QtWidgets.QPushButton("Generate Password")
         clear_button = QtWidgets.QPushButton("Clear")
         self.password_display_label = QtWidgets.QLabel()
+
+        # Connects button to clear fields function
         clear_button.clicked.connect(self.clear_fields)
 
+        # Sets range of spinbox to desired amount
         self.password_length_spinbox.setRange(1,15)
 
         # Adds the widgets to the layout
@@ -40,12 +43,20 @@ class PasswordGenerator(QtWidgets.QMainWindow):
         layout.addWidget(clear_button)
         layout.addWidget(self.password_display_label)
     
+        # Clears all of users input 
     def clear_fields(self):
         self.password_length_spinbox.clear()
         self.include_lowercase_checkbox.setChecked(False)
         self.include_uppercase_checkbox.setChecked(False)
         self.include_numbers_checkbox.setChecked(False)
         self.include_symbols_checkbox.setChecked(False)
+
+    def generate_password(self):
+        length = self.password_display_label_value()
+        if length > 15:
+            self.password_display_label.setText("Errpr: Password length cannot exceed 15.")
+        return 
+
 
         
 if __name__ == "__main__":
